2ed8f45abf4138433df33673060d024cbbc1552c
basarat <basaratali@gmail.com>
2013-09-11 18:46:44 +1000

    simple click demo

--- typescript/controllers/MainController.ts
+++ typescript/controllers/MainController.ts
@@ -1,9 +1,14 @@
 module Controllers{
     export class MainController{
         message = "asdf";
+        progress = 0; 
         constructor($scope,logService:LogService){
             $scope.vm = this;
             logService.log('Some log');
+        }
+
+        increaseProgress() {
+            this.progress += 10; 
         }
     }
 }
