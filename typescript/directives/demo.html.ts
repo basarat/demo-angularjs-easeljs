b7d2c11119374fe66940ffec6ca23bc289e55c1e
basarat <basaratali@gmail.com>
2013-09-11 18:42:17 +1000

    clean clean clean

--- typescript/directives/demo.html.ts
+++ typescript/directives/demo.html.ts
@@ -1,1 +1,1 @@
-module demo { export var html =  '<div class="well">     <div class="progress progress-striped progress-active">        <div class="bar" style="width: {{progress}}%"></div>    </div>    <div class="btn" ng-click="click()">Click me</div>    <div ng-transclude></div>    <input/></div>' } 
+module demo { export var html =  '<div class="well">     <div class="progress progress-striped progress-active">        <div class="bar" style="width: {{progress}}%"></div>    </div></div>' } 
